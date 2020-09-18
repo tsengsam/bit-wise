c9900515d7368043acfdbebe43ec3b9f6984fef0
Shan <coolmrsam3462521@gmail.com>
2020-09-19 00:17:53 +0800

    Implement question 3

---
+++ 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+
+const uint32_t D = 3;
+#define M ((uint64_t)(UINT64_C(0xFFFFFFFFFFFFFFFF) / (D) + 1))
+
+/* compute (n mod d) given precomputed M */
+uint32_t quickmod(uint32_t n)
+{   
+    uint64_t quotient = ((__uint128_t) M * n) >> 64;
+    return n - quotient * D;
+}
+
+void main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("%d\n", quickmod(n));
+}
