467cc52975b6fe21c0c8de2912fff951087e6690
Shan <coolmrsam3462521@gmail.com>
2020-09-24 22:50:51 +0800

    Implement question 1

--- 1.c
+++ 1.c
@@ -13,36 +13,35 @@
     return true;
 }*/
 
-/* Check the whole string in a time
- * size of string
- *
- *
- */
-bool is_ascii(const char str[], size_t size) {
-  if (size == 0)
-    return false;
-  int i = 0;
-  while ((i + 8) <= size) {
-    uint64_t payload;
-    memcpy(&payload, str + i, 8);
-    uint64_t MMM = 0x8080808080808080;
-    if (payload & MMM)
-      return false;
-    i++;
-  }
-  while (i < size) {
-    if (str[i] & 0x80)
-      return false;
-    i++;
-  }
-  return true;
+bool is_ascii(const char str[], size_t size)
+{
+    if (size == 0)
+        return false;
+    int i = 0;
+    while ((i + 8) <= size) {
+        uint64_t payload;
+        memcpy(&payload, str + i, 8);
+        uint64_t MMM = 0x8080808080808080;
+        if (payload & MMM)
+            return false;
+        i += 8;
+    }
+    while (i < size) {
+        if (str[i] & 0x80)
+            return false;
+        i++;
+    }
+    return true;
 }
-int main() {
-  char s[] = "abc*®+_|]";
-  if (is_ascii(s, 11)) {
-    printf("all characters valid\n");
-  } else {
-    printf("Not all valid\n");
-  }
-  return 0;
+int main()
+{
+    char str[] = "as12!@{}~~";
+    // char str[] = "AaÀ123}@`Ë";
+    printf("str = %s\n", str);
+    if (is_ascii(str, 10)) {
+        printf("All valid\n");
+    } else {
+        printf("Not all valid\n");
+    }
+    return 0;
 }
