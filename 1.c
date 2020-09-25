469b2ba9064d60853f076371d2e646790a8acc51
Shan <coolmrsam3462521@gmail.com>
2020-09-25 13:33:47 +0800

    Rewrite is_alphabet function

--- 1.c
+++ 1.c
@@ -3,31 +3,21 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+
 #define PACKED_BYTE(b) (((uint64_t)(b) & (0xff)) * 0x0101010101010101u)
-
-/*bool is_ascii(const char str[], size_t size)
-{
-    if (size == 0) return false;
-
-    for (int i = 0; i<size; i++) {
-        if (str[i] & 0x80) return false;
-    }
-    return true;
-}*/
 
 bool is_ascii(const char str[], size_t size)
 {
     if (size == 0)
         return false;
     int i = 0;
-    while ((i + 8) <= size) {
+    for (; i + 8 <= size; i += 8) {
         uint64_t payload;
         memcpy(&payload, str + i, 8);
-        uint64_t MMM = 0x8080808080808080;
-        if (payload & MMM)
+        if (payload & PACKED_BYTE(0x80))
             return false;
-        i += 8;
     }
+
     while (i < size) {
         if (str[i] & 0x80)
             return false;
@@ -36,52 +26,51 @@ bool is_ascii(const char str[], size_t size)
     return true;
 }
 
-bool is_alphabet(char str[], size_t size)
+bool is_alphabet(const char str[], size_t size)
 {
     if (size == 0)
         return false;
+
     int i = 0;
-    while ((i + 8) <= size) {
+    while (i + 8 <= size) {
         uint64_t payload;
-	memcpy(&payload, str + i, 8);
-	uint64_t A = payload + PACKED_BYTE(128 - 'A');
-	uint64_t Z = payload + PACKED_BYTE(127 - 'Z');
-	uint64_t upper_mask = (A^Z) & PACKED_BYTE(0x80);/*If one byte is 80 then the corresponding character is upper alphabet*/
-	uint64_t a = payload + PACKED_BYTE(128 - 'a');
-	uint64_t z = payload + PACKED_BYTE(127 - 'z' );
-	uint64_t lower_mask = (a ^ z) & PACKED_BYTE(0x80);
-	if ((upper_mask | lower_mask) ^ PACKED_BYTE(0x80))
-	    return false;
-	i += 8;
+        memcpy(&payload, str + i, 8);
+
+        uint64_t A = payload + PACKED_BYTE(128 - 'A');
+        uint64_t Z = payload + PACKED_BYTE(127 - 'Z');
+        uint64_t upper_mask =
+            (A ^ Z) & PACKED_BYTE(0x80); /*Should be PACKED_BYTE(0x80) */
+        uint64_t a = payload + PACKED_BYTE(128 - 'a');
+        uint64_t z = payload + PACKED_BYTE(127 - 'z');
+        uint64_t lower_mask =
+            (a ^ z) & PACKED_BYTE(0x80); /*Should be PACKED_BYTE(0x80), too*/
+        if ((upper_mask | lower_mask) ^ PACKED_BYTE(0x80))
+            return false;
+        i += 8;
     }
 
-    for (; i<size; i++) {
+    while (i < size) {
         if (str[i] < 65 || str[i] > 122 || (str[i] > 90 && str[i] < 97))
-	    return false;
+            return false;
+        i++;
     }
-
     return true;
-
 }
 
 int main()
 {
-    char str[] = "as12!@[}~~";
-    char str2[] = "Aaasdf@asi";
-    printf("str = %s\n", str);
-    if (is_ascii(str, 10)) {
-        printf("All valid\n");
-    } else {
-        printf("Not all valid\n");
+    char str[] = "qqqqqa®sd123";
+    char str2[] = "figil@gbjhdub";
+    if (is_ascii(str, strlen(str))) {
+        printf("All valid.\n");
+    }else {
+        printf("Not all valid.\n");
     }
 
-    printf("\nstr2 = %s\n", str2);
-    if (is_alphabet(str2, 10)) {
-        printf("All character are alphabets\n");
-    } else {
-        printf("Ohh, there are some other character.\n");
+    if (is_alphabet(str2, strlen(str2))) {
+        printf("All are alphabet~.\n");
     }
-        
-
-    return 0;
+    else {
+        printf("Not all alphabet...\n");
+    }
 }
